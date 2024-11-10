a846e92a583e2b371e9ee51c8e0d4c932a32c93d
yatharth-7022 <yatharth7022@gmail.com>
2024-11-10 14:50:57 +0530

    chaging the api callling method from xml to promises and displaying the neighbouring country as well

--- script.js
+++ script.js
@@ -3,34 +3,52 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`); //api name
-  request.send();
-
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const currency = Object.values(data.currencies)[0];
-    const countryCurrency = currency.name;
-
-    const html = `     
-        <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
-            <p class="country__row"><span>💰</span>${countryCurrency}</p>
-          </div>
-        </article>`;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  });
+const renderCountry = function (data, className = "") {
+  const currency = Object.values(data.currencies)?.[0];
+  const countryCurrency = currency.name;
+  const language = Object.values(data.languages)[0];
+  const html = `     
+    <article class="country  ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${countryCurrency}</p>
+      </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 };
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Neighbour not found (${response.status})`);
+      return response.json();
+    })
+    .then((data) => renderCountry(data[0], "neighbour"))
+    .catch((err) => {
+      console.error(`💥 ${err.message}`);
+      // Optionally display error to user
+      countriesContainer.insertAdjacentHTML(
+        "beforeend",
+        `<p class="error">💥 ${err.message}</p>`
+      );
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
 getCountryData("india");
-getCountryData("portugal");
-getCountryData("canada");
