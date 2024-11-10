2f7faad6f4c9f83dfae19082cd84d4cbaa9e4536
yatharth-7022 <yatharth7022@gmail.com>
2024-11-10 18:44:50 +0530

    adding async  await and then methods and also addded error handelling

--- script.js
+++ script.js
@@ -53,5 +53,13 @@ const getCountryData = function (country) {
     });
 };
 btn.addEventListener("click", () => {
-  getCountryData("australia");
+  getCountryData("india");
 });
+
+// const whereAmI = async function (country) {
+//   const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+//   const data = await res.json();
+//   console.log(data);
+//   renderCountry(data[0]);
+// };
+// whereAmI("india");
